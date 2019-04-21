c1c20859f407db02942788e5fa372716f8520533
Michael <print@3daddict.com>
2019-04-20 17:29:02 -0700

    img animation working

---
+++ app.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+const cycleImages = () => {
+    let images = $('#imageSet img');
+    let now = images.filter(':visible');
+    let next = now.next().length ? now.next() : images.first();
+    const speed = 2000;
+
+    now.fadeOut(speed);
+    next.fadeIn(speed);
+}
+
+$(() => {
+    setInterval(cycleImages, 2000);
+});
